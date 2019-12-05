b3b8967237b5954d0f3d5d47ce681001c425e610
Le Thi Cam Tien <thicamtien.le@users-iMac.localdomain>
2019-12-05 09:28:31 -0500

    add origin master

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    host: "localhcleardb.netst",
+    // port: 8889, // 8889 for mac
+    uname: "b6f6967263d264",
+    pword: "3169b54a", // leave blank for windows
+    database: "heroku_c600e604ec49fdf"
+}
+
+module.exports = config;
